4f1b6747ddceeefb3542c43137a1257d84dbaedf
arrrin <69157252+arrrin@users.noreply.github.com>
2023-09-18 15:15:32 +0700

    Update profile update

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,6 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 
-
 app.post('/register', async (req, res) => {
     try {
         const { email, password, goal, birthDate, gender, height, weight, weightGoal, activityLevel, eatType } = req.body;
@@ -51,12 +50,39 @@ app.post('/login', async (req, res) => {
             return res.status(401).json({ message: 'Authentication failed' });
         }
 
-        
+
         const token = jwt.sign({ userId: user._id }, 'your-secret-key', { expiresIn: '7d' });
 
         res.json({ token });
     } catch (error) {
         res.status(400).json({ message: error.message });
+    }
+});
+
+app.put('/profile', verifyToken, async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        const updatedFields = {
+            goal: req.body.goal,
+            birthDate: req.body.birthDate,
+            gender: req.body.gender,
+            height: req.body.height,
+            weight: req.body.weight,
+            weightGoal: req.body.weightGoal,
+            activityLevel: req.body.activityLevel,
+            eatType: req.body.eatType,
+            userImg: req.body.userImg,
+        };
+                const updatedProfile = await User.findByIdAndUpdate(userId, updatedFields, { new: true });
+
+        if (!updatedProfile) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.json({ message: 'User profile updated successfully', user: updatedProfile });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error', error: error.message });
     }
 });
 
