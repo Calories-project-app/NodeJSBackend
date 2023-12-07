00de0d8aa826bb45971e833a1bc9f1f46b5dd6de
arrrin <69157252+arrrin@users.noreply.github.com>
2023-12-07 21:12:37 +0700

    Develop: Water One day

--- routes/water-history.js
+++ routes/water-history.js
@@ -41,4 +41,61 @@ router.get('/waterHistory/:userId', verifyToken, async (req, res) => {
         res.status(500).json({ message: 'Internal server error', error: error.message });
     }
 });
+router.post("/waterHistory/oneDayStatistics", verifyToken, async (req, res) => {
+    try {
+      const { userId, date } = req.body;
+  
+      const userExists = await User.findById(userId);
+      if (!userExists) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      const startDate = new Date(date);
+      const endDate = new Date(date);
+      endDate.setDate(endDate.getDate() + 1);
+  
+      const foodHistory = await WaterHistory.find({
+        userId,
+        time: { $gte: startDate, $lt: endDate },
+      });
+      const waterHistory = await WaterHistory.find({
+        userId,
+        time: { $gte: startDate, $lt: endDate },
+      });
+      const totalLitre = waterHistory.reduce(
+        (sum, entry) => sum + (entry.intake || 0),
+        0
+      );
+      const totalCaloriesWater = waterHistory.reduce(
+        (sum, entry) => sum + (entry.calories || 0),
+        0
+      );
+      const totalCaffeine = waterHistory.reduce(
+        (sum, entry) => sum + (entry.caffeine || 0),
+        0
+      );
+      const totalSugar = waterHistory.reduce(
+        (sum, entry) => sum + (entry.sugar || 0),
+        0
+      );
+      const totalCaloriesFood = foodHistory.reduce(
+        (sum, entry) => sum + (entry.calories || 0),
+        0
+      );
+
+      const totalCalories = totalCaloriesFood + totalCaloriesWater;
+      res.status(200).json({
+        foodHistory,
+        waterHistory,
+        totalCalories,
+        totalLitre,
+        totalCaffeine,
+        totalSugar,
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Internal server error", error: error.message });
+    }
+  });
 module.exports = router;
