8c0942dfdf63c524110ad2b252886cb21c449aec
Arin Salabsi <69157252+arrrin@users.noreply.github.com>
2024-03-02 21:09:45 +0700

    Update friend routing

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,52 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const User = require("../models/User");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/addFriend', async (req, res) => {
+//getAllMyFriendList
+router.get('/friends', async (req, res) => {
+  try {
+    const userId = req.query.userId;
+    const users = await User.findById(userId).select('friends')
+
+    if (!users) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    let friendsDetails = [];
+    for (let key in users.friends) {
+      let friendId = users.friends[key];
+      const friendDetail = await User.findById(friendId).select('_id name');
+      if (friendDetail) { // Make sure the friend was found
+        friendsDetails.push(friendDetail);
+      }
+    }
+
+    res.status(200).json(friendsDetails);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error', error: error.message });
+  }
+});
+
+
+router.get('/friends/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
+
+    // Find users by name
+    const users = await User.find({ name: new RegExp(name, 'i') }).select('_id name userImg');
+
+    res.json(users);
+
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error', error: error.message });
+  }
+});
+
+router.post('/add-friend', async (req, res) => {
   try {
     const { currentUserId, friendIdToAdd } = req.body;
 
